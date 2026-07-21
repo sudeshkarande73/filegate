d0fd60118f18a9f8095a8685bbcb507744949268
sudeshkarande73 <sudeshkarande73@gmail.com>
2026-07-21 22:51:19 +0530

    Change API baseURL to new endpoint
    Updated baseURL to point to the new API endpoint.

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your local Node backend URL
+  baseURL: ' https://render.com/docs/web-services#port-binding', 
   withCredentials: true, // CRUCIAL: Tells the browser to send cookies along with requests
   headers: {
     'Content-Type': 'application/json',
