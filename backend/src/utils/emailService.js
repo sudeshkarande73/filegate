01fe3954d69155d7c77eac30cd7c87a08ba4c4a5
sudeshkarande73 <sudeshkarande73@gmail.com>
2026-07-22 23:41:01 +0530

    Update emailService.js

--- backend/src/utils/emailService.js
+++ backend/src/utils/emailService.js
@@ -5,13 +5,15 @@ const sendEmail = async (to, subject, text, html) => {
     console.log("EMAIL_USER:", process.env.EMAIL_USER);
     console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+   const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.verify();
     console.log("✅ SMTP connection successful");
