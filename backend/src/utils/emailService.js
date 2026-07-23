9fb036c1bce5d1a869455b57361bc1caf902bf3c
sudeshkarande73 <sudeshkarande73@gmail.com>
2026-07-23 18:55:56 +0530

    Brevo mail sender updated

--- backend/src/utils/emailService.js
+++ backend/src/utils/emailService.js
@@ -1,27 +1,36 @@
-const { Resend } = require("resend");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const client = SibApiV3Sdk.ApiClient.instance;
+
+client.authentications["api-key"].apiKey =
+  process.env.BREVO_API_KEY;
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
 const sendEmail = async (to, subject, text, html) => {
   try {
-    console.log("Sending email using Resend...");
+    const result = await apiInstance.sendTransacEmail({
+      sender: {
+        name: "FileGate Security",
+        email: "vaulteshare.noreply@gmail.com",
+      },
 
-    const response = await resend.emails.send({
-      from: "FileGate <onboarding@resend.dev>",
-      to,
+      to: [
+        {
+          email: to,
+        },
+      ],
+
       subject,
-      text,
-      html,
+      textContent: text,
+      htmlContent: html,
     });
 
-    console.log("Email sent successfully");
-    console.log(response);
-
-    return response;
-
-  } catch (error) {
-    console.error("Resend Error:", error);
-    throw error;
+    console.log("Email sent");
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 };
 
